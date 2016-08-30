ee3d19472ecac2ab62a9eaab6409e480d251076d
hkum22 <hkum22@delbahamas46576.sapient.com>
2016-08-30 11:15:28 +0530

    Second commit using comments...

--- EmployeeSpringProject/src/com/sahil/dao/EmployeeDao.java
+++ EmployeeSpringProject/src/com/sahil/dao/EmployeeDao.java
@@ -7,6 +7,6 @@ import com.sahil.bean.Employee;
 public interface EmployeeDao {
 	public int insertEmployee(Employee emp);
 	public List<Employee> showEmployees();
-	public int deleteEmployee(int e_id);
+	public int deleteEmployee(int e_id);   				// adding comment for master branch
 	public int updateEmployee(int e_id);
 }
