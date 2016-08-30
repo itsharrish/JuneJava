b10daa5deeed2ef5b5bf262525c21900edf88bd9
hkum22 <hkum22@delbahamas46576.sapient.com>
2016-08-30 11:58:05 +0530

    Third from pull

--- EmployeeSpringProject/src/com/sahil/dao/EmployeeDao.java
+++ EmployeeSpringProject/src/com/sahil/dao/EmployeeDao.java
@@ -5,7 +5,7 @@ import java.util.List;
 import com.sahil.bean.Employee;
 
 public interface EmployeeDao {
-	public int insertEmployee(Employee emp);
+	public int insertEmployee(Employee emp);			//Third from pull
 	public List<Employee> showEmployees();
 	public int deleteEmployee(int e_id);   				// adding comment for master branch
 	public int updateEmployee(int e_id);				// Second from pull trial
